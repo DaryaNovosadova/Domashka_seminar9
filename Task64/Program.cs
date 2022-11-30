91c65f56c7f2ea0cef0dd373042591edbed6a9eb
Darya <darya.novosadova@gmail.com>
2022-11-30 09:31:39 +0700

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if(number < 1) return;
+Console.WriteLine();
+int n = 1;
+void PrintNumber(int n, int number)
+{
+    if(n == number)
+    {
+        Console.Write("{0, 3}", number);
+        return;
+    }
+    else
+    {
+        Console.Write("{0, 3}", n);
+    }
+    PrintNumber(n +1, number);
+}
+
+for(int i = n; i <= number; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write("{0, 3}", i);
+    }
+} 
+
+
